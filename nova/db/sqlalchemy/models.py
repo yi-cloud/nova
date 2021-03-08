2cfe73adb42d95000eb8910182607356954c3540
zhangping <675046166@qq.com>
2021-03-08 22:16:46 +0800

    block_device_mapping增加volume_type字段

--- nova/db/sqlalchemy/models.py
+++ nova/db/sqlalchemy/models.py
@@ -626,6 +626,7 @@ class BlockDeviceMapping(BASE, NovaBase, models.SoftDeleteMixin):
 
     volume_id = Column(String(36))
     volume_size = Column(Integer)
+    volume_type = Column(String(255))
 
     image_id = Column(String(36))
 
